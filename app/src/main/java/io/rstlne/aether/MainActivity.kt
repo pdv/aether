aa45ca41b6a3f46978ed98a5d53543312f2fd3da
pdv <philipjdelvecchio@gmail.com>
2017-10-27 15:08:57 -0400

    Kill Pad

--- app/src/main/java/io/rstlne/aether/MainActivity.kt
+++ app/src/main/java/io/rstlne/aether/MainActivity.kt
@@ -1,35 +1,46 @@
 package io.rstlne.aether
 
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
-import android.view.ViewManager
-import android.widget.GridLayout
-import android.widget.TextView
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.jakewharton.rxbinding2.view.touches
-import io.reactivex.functions.Consumer
-import org.jetbrains.anko.backgroundResource
-import org.jetbrains.anko.button
-import org.jetbrains.anko.custom.ankoView
 import org.jetbrains.anko.dip
 import org.jetbrains.anko.gridLayout
-import org.jetbrains.anko.horizontalScrollView
+import org.jetbrains.anko.linearLayout
 import org.jetbrains.anko.margin
-import org.jetbrains.anko.scrollView
+import org.jetbrains.anko.spinner
 import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.view
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
+        val ROWS = 8
         val COLS = 6
+        val MAJOR = listOf(2, 2, 1, 2, 2, 2, 1)
+        val MINOR = listOf(2, 1, 2, 2, 1, 2, 2)
+        val BLUES = listOf(3, 2, 1, 1, 3, 2)
     }
 
     private lateinit var midi: RxMidi
 
-    private lateinit var pads: List<Pad>
+    private lateinit var pads: List<View>
+    private val notes: MutableList<Int> = (0..ROWS * COLS).map { 0 }.toMutableList()
+
+    private var root = 12
+        set(value) {
+            field = value
+            setScale(root, scale)
+        }
+    private var scale = MAJOR
+        set(value) {
+            field = value
+            setScale(root, scale)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,14 +50,56 @@ class MainActivity : AppCompatActivity() {
 
         verticalLayout {
             setBackgroundResource(R.color.gray)
+
+            linearLayout {
+
+                spinner {
+                    adapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, listOf("C", "C# / Db", "D", "D# / Eb", "E", "F", "F# / Gb", "G", "G# / Ab", "A", "A# / Bb", "B"))
+                    onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, idx: Int, p3: Long) {
+                            root = idx + 12
+                        }
+                        override fun onNothingSelected(p0: AdapterView<*>?) = Unit
+                    }
+                }
+
+                spinner {
+                    val options = listOf(
+                        "Major" to MAJOR,
+                        "Minor" to MINOR,
+                        "Blues" to BLUES
+                    )
+                    adapter = ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, options.map { it.first })
+                    onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, idx: Int, p3: Long) {
+                            scale = options[idx].second
+                        }
+                        override fun onNothingSelected(p0: AdapterView<*>?) = Unit
+                    }
+
+                }
+            }
+
             gridLayout {
                 columnCount = COLS
 
-                val pads = mutableMapOf<Int, Pad>()
-                (7 downTo 0).forEach { row ->
+                val pads = mutableMapOf<Int, View>()
+                (ROWS - 1 downTo 0).forEach { row ->
                     (0 until COLS).forEach { col ->
                         val idx = row * COLS + col
-                        val btn = pad(midi.output).lparams {
+                        val btn = view {
+
+                            touches()
+                                .unwrapMap {
+                                    when (it.action) {
+                                        MotionEvent.ACTION_DOWN -> MidiMessage.NoteOn(notes[idx], 127)
+                                        MotionEvent.ACTION_UP -> MidiMessage.NoteOff(notes[idx])
+                                        else -> null
+                                    }
+                                }
+                                .subscribe(midi.output)
+
+                        }.lparams {
                             margin = dip(2)
                             width = dip(64)
                             height = dip(64)
@@ -62,39 +115,16 @@ class MainActivity : AppCompatActivity() {
         setScale(12, MAJOR)
     }
 
-    fun setScale(root: Int, intervals: List<Int>) {
+    private fun setScale(root: Int, intervals: List<Int>) {
         val octaveSize = intervals.sum()
         val scaleLength = intervals.count()
         pads.forEachIndexed { index, pad ->
-            val effectiveIndex = if (index >= COLS) index - (index / COLS) * (COLS / 2) else index
+            val effectiveIndex = index - (index / COLS) * (COLS / 2)
             val interval = effectiveIndex % scaleLength
-            if (interval == 0) {
-                pad.setBackgroundResource(R.color.dark_blue)
-            }
+            pad.setBackgroundResource(if (interval == 0) R.color.dark_blue else R.color.blue)
             val octave = (effectiveIndex / scaleLength)
-            pad.midiNote = root + (octave * octaveSize) + (intervals.subList(0, interval).sum())
+            notes[index] = root + (octave * octaveSize) + (intervals.subList(0, interval).sum())
         }
-    }
-
-}
-
-fun ViewManager.pad(output: Consumer<MidiMessage>, init: Pad.() -> Unit = {}) = ankoView({ Pad(it, output) }, 0, init)
-
-class Pad(ctx: Context, output: Consumer<MidiMessage>) : View(ctx) {
-
-    var midiNote: Int = 60
-
-    init {
-        backgroundResource = R.color.blue
-        touches()
-            .unwrapMap {
-                when (it.action) {
-                    MotionEvent.ACTION_DOWN -> MidiMessage.NoteOn(midiNote, 127)
-                    MotionEvent.ACTION_UP -> MidiMessage.NoteOff(midiNote)
-                    else -> null
-                }
-            }
-            .subscribe(output)
     }
 
 }
