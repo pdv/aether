bb98b1752b110245c8ac3dce6e7586c76d8fd449
pdv <philipjdelvecchio@gmail.com>
2017-10-27 10:22:09 -0400

    Midi hookup

--- app/src/main/java/io/rstlne/aether/MainActivity.kt
+++ app/src/main/java/io/rstlne/aether/MainActivity.kt
@@ -1,47 +1,35 @@
 package io.rstlne.aether
 
-import android.content.Context
-import android.media.midi.MidiInputPort
-import android.media.midi.MidiManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
+import android.view.MotionEvent
+import com.jakewharton.rxbinding2.view.touches
 import org.jetbrains.anko.button
+import org.jetbrains.anko.gridLayout
 import org.jetbrains.anko.linearLayout
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var inputPort: MidiInputPort
+    private lateinit var rxMidi: RxMidi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val m = getSystemService(Context.MIDI_SERVICE) as MidiManager
-        val info = m.devices[0]
+        rxMidi = RxMidiImpl(this)
+        rxMidi.start()
 
-        m.openDevice(info, {
-            if (it == null) {
-                Log.e("MIDI", "Failed to open")
-            } else {
-                inputPort = it.openInputPort(0)
-            }
-
-        }, Handler(Looper.getMainLooper()))
-
-        linearLayout {
-            button {
-                setOnClickListener {
-                    val buffer = ByteArray(32)
-                    var numBytes = 0
-                    val channel = 3 // MIDI channels 1-16 are encoded as 0-15.
-                    buffer[numBytes++] = (0x90 + (channel - 1)).toByte() // note on
-                    buffer[numBytes++] = 60.toByte() // pitch is middle C
-                    buffer[numBytes++] = 127.toByte() // max velocity
-                    val offset = 0
-                    // post is non-blocking
-                    inputPort.send(buffer, offset, numBytes)
+        gridLayout {
+            columnCount = 4
+            (60..80).forEach { note ->
+                button {
+                    text = note.toString()
+                    touches()
+                        .unwrapMap { when (it.action) {
+                            MotionEvent.ACTION_DOWN -> MidiMessage.NoteOn(note, 127)
+                            MotionEvent.ACTION_UP -> MidiMessage.NoteOff(note)
+                            else -> null
+                        }}
+                        .subscribe(rxMidi.output)
                 }
             }
         }
--- app/src/main/java/io/rstlne/aether/RxMidi.kt
+++ app/src/main/java/io/rstlne/aether/RxMidi.kt
@@ -1,9 +1,14 @@
 package io.rstlne.aether
 
 import android.content.Context
+import android.media.midi.MidiInputPort
 import android.media.midi.MidiManager
+import android.os.Handler
+import android.os.Looper
 import com.jakewharton.rxrelay2.PublishRelay
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.functions.Consumer
+import io.reactivex.rxkotlin.addTo
 
 /**
  * Created by pdv on 10/27/17.
@@ -19,7 +24,7 @@ fun MidiMessage.toByteArray(channel: Int): ByteArray {
     buffer[0] = (when (this) {
         is MidiMessage.NoteOn -> 0x90
         is MidiMessage.NoteOff -> 0x80
-    } + (channel -1)).toByte()
+    } + channel).toByte()
     buffer[1] = when (this) {
         is MidiMessage.NoteOn -> key
         is MidiMessage.NoteOff -> key
@@ -32,18 +37,32 @@ fun MidiMessage.toByteArray(channel: Int): ByteArray {
 }
 
 interface RxMidi {
+    fun start()
     val output: Consumer<MidiMessage>
 }
 
 class RxMidiImpl(context: Context) : RxMidi {
 
-    override val output: Consumer<MidiMessage> = PublishRelay.create()
+    override val output: PublishRelay<MidiMessage> = PublishRelay.create()
 
     private val midiManager = context.getSystemService(Context.MIDI_SERVICE) as MidiManager
 
-    fun start() {
-
+    override fun start() {
+        // TODO: More than one device
+        val info = midiManager.devices[0]
+        midiManager.openDevice(info, {
+            routeOutput(it.openInputPort(0))
+        }, Handler(Looper.getMainLooper()))
     }
 
+    private val disposeBag = CompositeDisposable()
+
+    private fun routeOutput(inputPort: MidiInputPort) {
+        output
+            .map { it.toByteArray(3) }
+            .debug("MIDI")
+            .subscribe { inputPort.send(it, 0, 3) }
+            .addTo(disposeBag)
+    }
 
 }
