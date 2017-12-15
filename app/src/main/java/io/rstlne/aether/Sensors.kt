c416e08269ecf3bcc8caf4c294644b085b7fc0fb
pdv <philipjdelvecchio@gmail.com>
2017-12-15 14:38:23 -0500

    Generic sensor

--- app/src/main/java/io/rstlne/aether/Sensors.kt
+++ app/src/main/java/io/rstlne/aether/Sensors.kt
@@ -12,32 +12,50 @@ import io.reactivex.functions.Consumer
  * Created by pdv on 12/15/17.
  */
 
-typealias MidiSensor = (SensorEvent) -> Set<MidiMessage>
+interface MidiSensor {
+    val type: Int
+    fun eventToMidi(event: SensorEvent): Set<MidiMessage>
+}
 
-// [-10, 10] -> [0, 127]
-private val Float.normalized: Int get() = ((this + 10) * (127f / 20)).toInt()
+object AccelerometerMidiSensor: MidiSensor {
+
+    // [-10, 10] -> [0, 127]
+    private val Float.normalized: Int get() = ((this + 10) * (127f / 20)).toInt()
+
+    override val type: Int = Sensor.TYPE_ACCELEROMETER
+
+    override fun eventToMidi(event: SensorEvent): Set<MidiMessage> {
+        return (0..2).map { MidiMessage.Control(it, event.values[it].normalized) }.toSet()
+    }
+
+}
+
+data class Snsr(
+    val midiSensor: MidiSensor,
+    val listener: RxSensor,
+    val sensor: Sensor
+)
 
 class Sensors(
     private val sensorManager: SensorManager,
     private val midiOut: Consumer<MidiMessage>
 ) {
-
-    private val accelerometerListener = RxSensor()
-    private val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-    private val accelerometerMapper: MidiSensor = { event ->
-        (0..2).map { MidiMessage.Control(it, event.values[it].normalized) }.toSet()
-    }
+    private val sensors: List<Snsr> = listOf<MidiSensor>(
+        AccelerometerMidiSensor
+    ).map { Snsr(it, RxSensor(), sensorManager.getDefaultSensor(it.type))}
 
     fun start() {
-        accelerometerListener.events
-            .map(accelerometerMapper)
-            .flatMap { Observable.fromIterable(it) }
-            .subscribe(midiOut)
-        sensorManager.registerListener(accelerometerListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensors.forEach { (midiSensor, listener, sensor) ->
+            listener.events
+                .map(midiSensor::eventToMidi)
+                .flatMap { Observable.fromIterable(it) }
+                .subscribe(midiOut)
+            sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        }
     }
 
     fun stop() {
-        sensorManager.unregisterListener(accelerometerListener)
+        sensors.forEach { sensorManager.unregisterListener(it.listener) }
     }
 
 }
